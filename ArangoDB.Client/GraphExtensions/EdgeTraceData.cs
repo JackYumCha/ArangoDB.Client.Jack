f8fd11c92016cc0f1ed9742d2fecfc44dac36701
JackYumCha <jackdatascience@gmail.com>
2018-04-02 03:38:53 +1000

    setup

---
+++ ArangoDB.Client/GraphExtensions/EdgeTraceData.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace ArangoDB.Client
+{
+    /// <summary>
+    /// the edge trace data for graph
+    /// </summary>
+    public class EdgeTraceData
+    {
+        /// <summary>
+        /// the vertex at the traced level
+        /// </summary>
+        public JObject Vertex { get; set;}
+        /// <summary>
+        /// the edge at the traced level
+        /// </summary>
+        public EdgeBase Edge { get; set; }
+    }
+}
